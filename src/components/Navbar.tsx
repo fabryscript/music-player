76927fbeed8ea488b24d317897b4ccd536351c39
fabry-js <vivaldifabrizio10@gmail.com>
2023-09-02 18:38:14 +0200

    feat: :sparkles: add navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,29 @@
-import Image from 'next/image'
-import React from 'react'
-import { FiMenu } from "react-icons/fi";
+"use client";
 
-import logo from "../../public/logo.png";
+import React from "react";
+import { IconContext } from "react-icons";
+import { CgMenuLeftAlt, CgSearch } from "react-icons/cg";
 
 function Navbar() {
   return (
-    <div className='grid grid-cols-6 m-4 gap-y-4'>
-      <Image
-        src={logo}
-        alt='NextCommerce logo'
-        width={50}
-        height={50}
-        className='col-start-3 col-span-2 justify-self-center'
-      />
-      <button className='col-start-6 col-span-2 w-[40px] h-[40px] self-center justify-self-center flex flex-row items-center justify-center bg-quaternary text-quinary p-1 rounded-full'>
-        <FiMenu />
-      </button>
-    </div>
-  )
+    <nav className="grid grid-cols-6 mx-4 mt-12 gap-y-4 text-secondary relative">
+      <IconContext.Provider value={{ size: "28" }}>
+        <div className="absolute w-[200px] h-[200px] -left-[53px] -top-[178px] blur-[400px] bg-[#fa11ff]" />
+        <button className="w-[50px] h-[50px] bg-[#fff]/5 backdrop-blur-[50px] rounded-lg flex justify-center items-center">
+          <CgMenuLeftAlt />
+        </button>
+        <div className="col-start-2 col-span-5 flex flex-row items-center gap-x-3 h-[50px] bg-[#fff]/5 backdrop-blur-[50px] rounded-lg font-light px-5">
+          <div className="opacity-50">
+            <CgSearch />
+          </div>
+          <input
+            className="bg-transparent focus:outline-none placeholder:opacity-50 placeholder:text-white text-base"
+            placeholder="Search"
+          />
+        </div>
+      </IconContext.Provider>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
