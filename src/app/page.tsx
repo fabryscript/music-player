5d42ffc523c3ee10309bd6551919473e12b015d9
fabry-js <vivaldifabrizio10@gmail.com>
2023-09-02 18:39:14 +0200

    feat: :sparkles: render all sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
+"use client";
+
+import BottomNav from "@/components/BottomNav";
 import Navbar from "@/components/Navbar";
+import Discover from "@/components/Discover";
+import RecentlyPlayed from "@/components/RecentlyPlayed";
+import { useMusic } from "@/stores/useMusic";
 
 export default function Home() {
+  const { isPlaying } = useMusic()
+
   return (
-    <main>
+    <main className={`${isPlaying ? "mb-[200px]" : "mb-[100px]"}`}>
       <Navbar />
-      
+      <Discover />
+      <RecentlyPlayed />
+      <BottomNav />
     </main>
   )
 }
