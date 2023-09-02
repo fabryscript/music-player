1782ace3e7a4c69ca72dd26ca12b14be85c53de3
fabry-js <vivaldifabrizio10@gmail.com>
2023-09-02 19:14:00 +0200

    feat: :sparkles: discover songs can now play them

--- src/components/Discover.tsx
+++ src/components/Discover.tsx
@@ -11,33 +11,66 @@ import sirio from "../../public/covers/sirio-lazza.jpeg";
 import barbie from "../../public/covers/barbie-world.jpeg";
 import ditg from "../../public/covers/ditg-zaq.jpeg";
 
-import { BsPlayFill } from "react-icons/bs";
+import { BsPauseFill, BsPlayFill } from "react-icons/bs";
 import { IconContext } from "react-icons";
+import { useMusic } from "@/stores/useMusic";
 
 const discoverItems = [
   {
     albumTitle: "Meet The Woo 2",
     author: "Pop Smoke",
     cover: mtw2,
+    discoverSong: {
+      songName: "Shake The Room (feat. Quavo)",
+      author: "Pop Smoke, Quavo",
+      cover: mtw2,
+      songPath: "songs/str-pop.mp3",
+    },
   },
   {
     albumTitle: "SIRIO",
     author: "Lazza",
     cover: sirio,
+    discoverSong: {
+      songName: "TOPBOY (feat. Noyz Narcos)",
+      author: "Lazza, Noyz Narcos",
+      cover: sirio,
+      songPath: "songs/tpb-lazza.mp3",
+    },
   },
   {
     albumTitle: "Barbie World (with Aqua) [From Barbie The World]",
     author: "Nicki Minaj",
     cover: barbie,
+    discoverSong: {
+      songName: "Barbie World (with Aqua) [From Barbie The World]",
+      author: "Nicki Minaj, Ice Spice",
+      cover: barbie,
+      songPath: "songs/bw-nicki.mp3",
+    },
   },
   {
     albumTitle: "Dance In In The Game",
     author: "ZAQ",
     cover: ditg,
+    discoverSong: {
+      songName: "Dance In The Game",
+      author: "ZAQ",
+      cover: ditg,
+      songPath: "songs/ditg-zaq.mp3",
+    },
   },
 ];
 
 function Discover() {
+  const {
+    addSong,
+    isPlaying,
+    currentlyPlaying: { infos, song },
+    pause,
+    play,
+  } = useMusic();
+
   return (
     <IconContext.Provider value={{ size: "28" }}>
       <Grid className="mt-5">
@@ -49,30 +82,66 @@ function Discover() {
             grabCursor
             spaceBetween={25}
           >
-            {discoverItems.map(({ albumTitle, author, cover }, i) => (
-              <SwiperSlide key={albumTitle} className={`max-w-[250px]`}>
-                <div className="flex w-full h-[200px] relative">
-                  <Image
-                    src={cover}
-                    width={250}
-                    height={200}
-                    alt={`${albumTitle} album cover`}
-                    className="object-cover rounded-3xl"
-                  />
-                  <div className="flex flex-row absolute text-white bottom-[10px] left-2 right-2 rounded-2xl p-4 justify-between bg-[#fff]/5 backdrop-blur-[50px]">
-                    <div className="flex flex-col">
-                      <h5 className="text-base">
-                        {albumTitle.substring(0, 18)}
-                      </h5>
-                      <p className="text-xs opacity-70">{author}</p>
+            {discoverItems.map(
+              ({
+                albumTitle,
+                author,
+                cover,
+                discoverSong: {
+                  songPath,
+                  author: songAuthor,
+                  cover: songCover,
+                  songName,
+                },
+              }) => {
+                const isThisSongPlaying = infos?.songPath === songPath;
+                return (
+                  <SwiperSlide key={albumTitle} className={`max-w-[250px]`}>
+                    <div className="flex w-full h-[200px] relative">
+                      <Image
+                        src={cover}
+                        width={250}
+                        height={200}
+                        alt={`${albumTitle} album cover`}
+                        className="object-cover rounded-3xl"
+                      />
+                      <div className="flex flex-row absolute text-white bottom-[10px] left-2 right-2 rounded-2xl p-4 justify-between bg-[#fff]/5 backdrop-blur-[50px]">
+                        <div className="flex flex-col">
+                          <h5 className="text-base">
+                            {albumTitle.substring(0, 18)}
+                          </h5>
+                          <p className="text-xs opacity-70">{author}</p>
+                        </div>
+                        <button
+                          onClick={() => {
+                            if (isThisSongPlaying && isPlaying) {
+                              pause();
+                            } else if (isThisSongPlaying) {
+                              play()
+                            } else {
+                              addSong(new Howl({ src: songPath }), {
+                                author: songAuthor,
+                                cover: songCover,
+                                songName,
+                                songPath,
+                              });
+                              play();
+                            }
+                          }}
+                          className="bg-white rounded-full flex justify-center items-center text-primary w-[40px] h-[40px]"
+                        >
+                          {isThisSongPlaying && isPlaying ? (
+                            <BsPauseFill />
+                          ) : (
+                            <BsPlayFill />
+                          )}
+                        </button>
+                      </div>
                     </div>
-                    <button className="bg-white rounded-full flex justify-center items-center text-primary w-[40px] h-[40px]">
-                      <BsPlayFill />
-                    </button>
-                  </div>
-                </div>
-              </SwiperSlide>
-            ))}
+                  </SwiperSlide>
+                );
+              }
+            )}
           </Swiper>
         </div>
       </Grid>
